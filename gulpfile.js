4b0ee9043f13781f750e488295c05c825ac79d12
Ivan Kuzmin <vania_kuzmin@mail.ru>
2020-11-27 11:50:17 +0300

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,44 @@
-var gulp       = require('gulp'),
-	browserSync  = require('browser-sync'),
-  del          = require('del'),
-	imagemin     = require('gulp-imagemin'),
-	pngquant     = require('imagemin-pngquant'),
-  cache        = require('gulp-cache'),
+const gulp     = require('gulp'),
+  sass         = require('gulp-sass'),
+  autoprefixer = require('autoprefixer'),
   inlineCss    = require('gulp-inline-css'),
-  htmlmin      = require('gulp-htmlmin');
+  connect      = require('gulp-connect')
+  del          = require('del');
 
-gulp.task('browser-sync', function() {
-	browserSync({
-		server: {
-			baseDir: 'app',
-		},
-		notify: false,
-	});
+gulp.task('sass', function(callback) {
+  return gulp.src('app/**/*.scss', { base: './app' })
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('app'));
 });
 
-gulp.task('code', function() {
-	return gulp.src('app/**/*')
-	  .pipe(browserSync.reload({ stream: true }))
+gulp.task('inlinecss', function() {
+  return gulp.src('app/**/*.html')
+    .pipe(
+      inlineCss({
+        applyStyleTags: true,
+        applyLinkTags: true,
+        removeStyleTags: true,
+        removeLinkTags: true,
+        removeHtmlSelectors: true,
+        applyTableAttributes: true,
+        preserveMediaQueries: true,
+      })
+    )
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
 });
 
-gulp.task('inline-css', async function() {
-  return gulp.src('app/**/*.html', { base: './app' })
-    .pipe(inlineCss({
-      applyStyleTags: true,
-      applyLinkTags: true,
-      removeStyleTags: true,
-      removeLinkTags: true,
-      removeHtmlSelectors: true,
-      applyTableAttributes: true,
-      preserveMediaQueries: true,
-    }))
-    .pipe(gulp.dest('dist'));
+gulp.task('connect', async function() {
+  connect.server({
+    port: 8000,
+    root: 'dist',
+    livereload: true,
+  });
 });
 
-gulp.task('clean', async function() {
-	return del.sync('dist');
+gulp.task('watch', async function() {
+  gulp.watch(['app/**/*', '!app/**/*.css'], gulp.series('build'));
 });
 
-gulp.task('img', async function() {
-	return gulp.src('app/**/img/**/*', { base: './app' })
-		.pipe(cache(imagemin({
-      interlaced: true,
-      progressive: true,
-      use: [pngquant()]
-    })))
-		.pipe(gulp.dest('dist'));
-});
-
-gulp.task('prebuild', async function() {
-  return gulp.src('app/**/*.html', { base: './app' })
-    .pipe(gulp.dest('dist'));
-});
-
-// gulp.task('optimize', async function() {
-//   return gulp.src('app/**/*.html', { base: './app' })
-//     .pipe(htmlmin())
-//     .pipe(gulp.dest('dist'));
-// });
-
-gulp.task('clear', function (callback) {
-	return cache.clearAll();
-});
-
-gulp.task('watch', function() {
-	gulp.watch('app', gulp.parallel('code'));
-});
-gulp.task('default', gulp.parallel('browser-sync', 'inline-css', 'watch'));
-gulp.task('build', gulp.parallel('prebuild', 'clean', 'img', 'inline-css'));
+gulp.task('default', gulp.series('connect', 'watch'));
+gulp.task('build', gulp.series('sass', 'inlinecss'));
