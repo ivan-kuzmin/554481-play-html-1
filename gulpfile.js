137e4e5861395be5eda26da583c73c6373b02320
Ivan Kuzmin <vania_kuzmin@mail.ru>
2020-12-20 17:02:31 +0300

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const gulp     = require('gulp'),
   removeFiles  = require('gulp-remove-files')
   htmlmin      = require('gulp-htmlmin');
 
+const IS_DEV = process.env.NODE_ENV !== 'production';
+
 gulp.task('sass', function(callback) {
   return gulp.src('app/**/*.scss', { base: './app' })
     .pipe(sass({ outputStyle: 'expanded' }))
@@ -20,7 +22,7 @@ gulp.task('inlineCss', function() {
         applyLinkTags: true,
         removeStyleTags: true,
         removeLinkTags: true,
-        removeHtmlSelectors: true,
+        removeHtmlSelectors: !IS_DEV,
         applyTableAttributes: true,
         preserveMediaQueries: true,
       })
@@ -30,8 +32,25 @@ gulp.task('inlineCss', function() {
     .pipe(connect.reload());
 });
 
-gulp.task('clearCss', async function () {
-  return gulp.src('app/*/*.css')
+gulp.task('html-css', function() {
+  return gulp.src(['app/**/*.html', 'app/**/*.css'])
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('images', async function() {
+  return gulp.src('app/**/*.png')
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('removeCss', async function () {
+  return gulp.src(['app/*/*.css', 'dist/*/*.css'])
+    .pipe(removeFiles());
+});
+
+gulp.task('clean', async function () {
+  return gulp.src('dist/**/*')
     .pipe(removeFiles());
 });
 
@@ -48,4 +67,5 @@ gulp.task('watch', async function() {
 });
 
 gulp.task('default', gulp.series('connect', 'watch'));
-gulp.task('build', gulp.series('sass', 'inlineCss', 'clearCss'));
+
+gulp.task('build', gulp.series('clean' , 'images', 'sass', IS_DEV ? 'html-css' : ['inlineCss', 'removeCss']));
